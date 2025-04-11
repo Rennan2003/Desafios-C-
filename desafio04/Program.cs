124274da6cca52db95d1186d85bf175c111ce583
Rennan2003 <rsilva@provertec.com.br>
2025-04-11 17:53:24 -0300

    Preparação para desafios de segunda

---
+++ desafio04/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Desafio04;
+
+ //1. Crie um programa com 2 valores do tipo **`double`** já declarados ****que retorne:
+  //- A soma entre esses dois números;
+  //- A subtração entre os dois números;
+  //- A multiplicação entre os dois números;
+  //- A divisão entre os dois números (vale uma verificação se o segundo número é 0!);
+  //- A média entre os dois números. 
