a5924b75f5188292ae69695093071999a9800d99
Rennan2003 <rsilva@provertec.com.br>
2025-04-14 13:58:39 -0300

    funciona

--- desafio05/obj/Debug/net9.0/desafio05.AssemblyInfo.cs
+++ desafio05/obj/Debug/net9.0/desafio05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("desafio05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c42cd24bf2a0b100de047626e585b0e35e06530")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75499fc05204893da74fb19c600fca37ec42868a")]
 [assembly: System.Reflection.AssemblyProductAttribute("desafio05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("desafio05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
